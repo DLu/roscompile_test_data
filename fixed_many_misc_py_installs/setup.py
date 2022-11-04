1441a64d1bf15212ca1a2a925f32f899a87bb692
David V. Lu <davidvlu@gmail.com>
2022-11-04 11:08:57 -0400

    Misc installs

---
+++ fixed_many_misc_py_installs/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from glob import glob
+
+package_name = 'my_minimal_python'
+
+setup(
+    name=package_name,
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/my_minimal_python']),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+    ],
+)
