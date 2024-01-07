1895bed512f406c4b646ae823318f0ef487b8057
David V. Lu <davidvlu@gmail.com>
2024-01-06 22:21:07 -0500

    Missing resource file

--- fixed_cpp_py_pkg/setup.py
+++ fixed_cpp_py_pkg/setup.py
@@ -6,6 +6,9 @@ package_name = 'my_cpp_py_pkg'
 
 setup(
     name=package_name,
-    data_files=[('share/' + package_name, ['package.xml'])],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/my_cpp_py_pkg']),
+    ],
     packages=[package_name],
 )
